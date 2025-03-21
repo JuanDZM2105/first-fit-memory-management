d56efa330b6935393081f6b86fba66fb60c7b916
JuanDZM2105 <juandzm2105@gmail.com>
2025-03-21 17:53:55 -0500

    fix: index problem

--- first_fit.py
+++ first_fit.py
@@ -12,14 +12,12 @@ def first_fit(memory, required, index):
 
             new_base = base + required
             new_limit = limit - required
-            new_index = (pos+1) % len(memory)
 
             if new_limit > 0:
                 memory[pos] = (new_base, new_limit)
             else:
-                new_index = pos
                 memory.pop(pos)
 
-            return  (memory, new_base, new_limit, new_index if memory else 0)
+            return  (memory, new_base, new_limit, pos)
 
     return  None
